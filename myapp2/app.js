76576d873c908287f948b5e52580dd9bb2b9e814
Nikos <niqos89@gmail.com>
2025-03-21 21:14:21 +0200

    node examples, apps

---
+++ myapp2/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express();
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("This is the home page")
+})
+
+
+app.post("/user", (req, res) => {
+  let data = req.body
+
+  console.log(data)
+  res.send("User Page")
+})
+
+
+
+app.listen(port, () => {
+  console.log("Server is up!")
+})
