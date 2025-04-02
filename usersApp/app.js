7aa0b995cb486990b65efea38b60e2e05d3bb89e
Nikos <niqos89@gmail.com>
2025-04-02 20:59:24 +0300

    feat: swagger added

--- usersApp/app.js
+++ usersApp/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json())
+
+const swaggerUI = require("swagger-ui-express")
+const swaggerDocument = require("./swagger")
+
 const user = require("./routes/user.routes")
 const userProduct = require("./routes/user.products.routes")
 const auth = require("./routes/auth.routes")
@@ -9,5 +13,7 @@ const auth = require("./routes/auth.routes")
 app.use("/api/auth", auth)
 app.use("/api/users", user);
 app.use("/api/user-product", userProduct)
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument.options))
 
 module.exports = app
