9ee24859488cddef1e08e2ce2cfbf089a492062c
Nikos <niqos89@gmail.com>
2025-05-12 20:32:11 +0300

    fix: changed cors

--- usersApp/app.js
+++ usersApp/app.js
@@ -16,7 +16,7 @@ const auth = require("./routes/auth.routes")
 app.use(cors({
   // ΔΕΝ ΕΙΝΑΙ ΚΑΛΗ ΠΡΑΚΤΙΚΗ ΤΟ ΑΣΤΕΡΑΚΙ, ΚΑΛΟ ΕΙΝΑΙ ΝΑ ΞΕΡΟΥΜΕ ΚΑΙ ΝΑ ΔΗΛΩΣΟΥΜΕ ΤΟ PORT
   // origin: "*"
-  origin: ["http://localhost:8000"]
+  origin: ["http://localhost:4200"]
 }))
 
 app.use("/api/auth", auth)
